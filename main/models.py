890d78c39bf808ba7191b93f2427664b96192be3
IrvanRizqy <irvan.rizqy.k@gmail.com>
2023-09-10 21:38:00 +0700

    update 1

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    amount = models.IntegerField()
     date_added = models.DateField(auto_now_add=True)
     price = models.IntegerField()
     description = models.TextField()
